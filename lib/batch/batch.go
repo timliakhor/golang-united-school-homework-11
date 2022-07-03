a69cc2976db53dcb50d531ca4019ce5711276f83
Timofei Liakhor <timofei.liakhor@gmail.com>
2022-07-03 15:38:35 +0400

    golang-united-school-homework-11

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -13,6 +14,32 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
+var mx sync.Mutex
+
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var i int64
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, pool)
+
+	for i = 0; i < n; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		i := i
+		go func() {
+			defer wg.Done()
+			user := getOne(i)
+			<-sem
+			mtxSynchronizedAppender(user, &res)
+		}()
+	}
+
+	wg.Wait()
+
+	return res
+}
+
+func mtxSynchronizedAppender(usr user, res *[]user) {
+	mx.Lock()
+	*res = append(*res, usr)
+	mx.Unlock()
 }
